840fc0e1b824d5f859ee8e617acf1a4b2d844753
Frank Long <v-frlon@microsoft.com>
2024-10-14 14:06:37 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
+using Test.DAL.Models;
+
+using var db = new TestDataContext();
+
+var tag = new Tag { TagName = "Test" };
+db.Add(tag);
+
+var post = new Post { PostName = "Test" };
+db.Add(post);
+
+post.Tags.Add(tag);
+db.SaveChanges();
+
+var posts = db.Posts
+    .Include(p => p.Tags)
+    .ToList();
+
+foreach (var p in posts)
+{
+    Console.WriteLine($"Post: {p.PostName}, Tag Count {p.Tags.Count}");
+}
+
+
+
